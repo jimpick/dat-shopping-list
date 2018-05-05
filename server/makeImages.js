5703adfe5f84a04926029aedbf699182046da81e
Jim Pick <jim@backblend.com>
2018-05-05 13:31:17 -0700

    Use phantomjs to render pngs from svgs
    I tried about 5 different libraries to find one that worked.

--- server/makeImages.js
+++ server/makeImages.js
@@ -1,34 +1,18 @@
-const gm = require('gm').subClass({imageMagick: true})
+const svgexport = require('svgexport')
 
 module.exports = makeImages
 
 function makeImages (cb) {
   console.log('Making images')
   const base = 'dat-shopping-list'
-  makePngFromSvg(base, 16)
-    .then(makePngFromSvg(base, 32))
-    .then(makePngFromSvg(base, 96))
-    .then(makePngFromSvg(base, 120))
-    .then(makePngFromSvg(base, 152))
-    .then(makePngFromSvg(base, 167))
-    .then(makePngFromSvg(base, 180))
-    .then(makePngFromSvg(base, 192))
-    .then(makePngFromSvg(base, 196))
-    .then(makePngFromSvg(base, 512))
-    .then(cb)
-    .catch(cb)
-}
+  const sizes = [16, 32, 96, 120, 152, 167, 180, 192, 196, 512]
 
-function makePngFromSvg (name, size) {
-  const promise = new Promise((resolve, reject) => {
-    gm(`./static/img/${name}.svg`)
-      .resize(size, size)
-      .write(`./.data/img/${name}-${size}.png`, err => {
-        if (err) {
-          return reject(err)
-        }
-        resolve()
-      })
-  })
-  return promise
+  svgexport.render({
+    input: [ `./static/img/${base}.svg` ],
+    output: sizes.map(size => [
+      `./.data/img/${base}-${size}.png`,
+      `${size}:`
+    ]),
+    cwd: process.cwd()
+  }, cb)
 }
